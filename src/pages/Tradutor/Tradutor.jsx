f0ceb379a0ef6e627224f483b94d6f644f4e8033
Dudxxx <130068099+Dudxxx@users.noreply.github.com>
2024-10-16 18:30:10 -0300

    .

--- src/pages/Tradutor/Tradutor.jsx
+++ src/pages/Tradutor/Tradutor.jsx
@@ -1,16 +1,25 @@
 import styles from './Tradutor.module.css';
 import { Pagina } from "../Home/Pagina";
-import { useState } from "react";
-import { useRef} from "react";
+import { useState, useRef } from "react";
 
 export function Tradutor() {
     const referencia = useRef(0);
     const [charCount, setCharCount] = useState(0);
+    const [translatedText, setTranslatedText] = useState('');
+    const [inputText, setInputText] = useState('');
 
     function atualizaContador(event) {
         const texto = event.target.value;
         referencia.current = texto.length;
         setCharCount(texto.length);
+        setInputText(texto);
+    }
+
+    async function fetchTranslation() {
+        const encodedText = encodeURIComponent(inputText);
+        const response = await fetch(`https://api.mymemory.translated.net/get?q=${encodedText}&langpair=pt|en`);
+        const data = await response.json();
+        setTranslatedText(data.responseData.translatedText);
     }
 
     return (
@@ -26,14 +35,15 @@ export function Tradutor() {
                             onChange={atualizaContador}
                             maxLength={250}
                         />
-                        <button className={styles.button}>Traduzir</button>
-                        <p className={styles.contador} > {charCount}/250</p>
+                        <button className={styles.button} onClick={fetchTranslation}>Traduzir</button>
+                        <p className={styles.contador}>{charCount}/250</p>
                     </div>
                     <div className={styles.inputGroup}>
                         <h2>Inglês</h2>
                         <textarea
                             className={styles.textarea}
                             placeholder="Tradução"
+                            value={translatedText}
                             readOnly
                         />
                     </div>
